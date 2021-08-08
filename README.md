98d24a9cfcba6b693be0ab1a569e94fd396959e3
TacticalShroom <gabrielschallock@icloud.com>
2021-08-07 23:50:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arcania-Reborn
+ WIP - MMO RPG minecraft plugin for Arcania Reborn, a fun fantasy experience brought to you by Knightways!
---
+++ src/com/tacticalshroom/arcania/Main.java
@@ -0,0 +1,21 @@
+package com.tacticalshroom.arcania;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    Main plugin = null;
+
+    public void onEnable()  {
+        plugin = this;
+    }
+
+    public void onDisable() {
+
+
+
+
+
+        plugin = null;
+    }
+}
